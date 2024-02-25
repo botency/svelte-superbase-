c8128370edeea6a66392f35c91d56f32c769d0b0
botency <135847090+botency@users.noreply.github.com>
2024-02-25 16:25:43 -0500

    Delete src directory

--- src/routes/home/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  // let name = $page.data.session.user.user_metadata.name;
-</script>
-
-<!-- <p class="mt-16 text-6xl text-indigo-900">Welcome to marketplace <span class="">{name}</span>!</p> -->
-<p class="mt-16 text-6xl text-indigo-900">Welcome to marketplace </p>
