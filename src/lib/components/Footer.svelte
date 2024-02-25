94af3f507167265d8b5c240a62ba0c1534f6e355
botency <135847090+botency@users.noreply.github.com>
2024-02-25 16:26:45 -0500

    Add files via upload

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="items-center py-8 text-indigo-900 footer">
+    <aside class="items-center grid-flow-col">
+      <p>Copyright © 2024 - All right reserved</p>
+    </aside> 
+    <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <a class="underline underline-offset-4" href="" target="_blank">  Github</a>
+    </nav>
+  </footer>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  
+  import { page } from "$app/stores";
+  let name = "";
+  
+  if (!!$page.data.session){
+    name = $page.data.session.user.email;
+  }
+</script>
+
+<p class="mt-16 text-6xl text-indigo-900">Welcome to marketplace {name}! </p>
