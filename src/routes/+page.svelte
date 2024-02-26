6afe67577c8fdd177c52d6e10f349826538341f2
Muhammad Zeeshan <xexhan17@gamil.com>
2024-02-26 10:18:14 +0500

    redirect bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,31 @@
 <script>
+  export let data;
+
+  $: if (data.session) {
+    window.location.reload();
+  }
 </script>
 
+<div class="px-4 py-6">
+  <p
+    class="mb-16 text-2xl text-center text-indigo-900 md:text-left md:text-6xl"
+  >
+    The ultimate hub for people to buy, sell, and connect over classifieds,
+    tailored for your job life - available anywhere, anytime.
+  </p>
 
-    <div class="px-4 py-6">
-      <p class="mb-16 text-2xl text-center text-indigo-900 md:text-left md:text-6xl">
-        The ultimate hub for people to buy, sell, and connect over classifieds,
-        tailored for your job life - available anywhere, anytime.
-	  </p>
-
-      <div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-        <!-- Main sections will stack vertically on mobile, and horizontally on wider screens -->
-        <div class="flex-1">
-			<div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">Sell Item</div>
-        </div>
-        <div class="flex-1">
-			<div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">Request Item</div>
-        </div>
-        <div class="flex-1">
-			<div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">Academic Services</div>
-        </div>
+  <div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+    <!-- Main sections will stack vertically on mobile, and horizontally on wider screens -->
+    <div class="flex-1">
+      <div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">Sell Item</div>
+    </div>
+    <div class="flex-1">
+      <div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">Request Item</div>
+    </div>
+    <div class="flex-1">
+      <div class="bg-[#1C1041] text-[#EDEFF4] p-4 rounded-md">
+        Academic Services
       </div>
     </div>
+  </div>
+</div>
